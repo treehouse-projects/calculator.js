4e189aa6ef47accd0357a5c406fd03fd1e20e8ed
Andrew Chalkley <andrew@teamtreehouse.com>
2016-09-12 13:07:24 -0700

    Fix typo in tests

--- test/test_calculator_multiplication.js
+++ test/test_calculator_multiplication.js
@@ -5,7 +5,7 @@ const {assert} = require('chai');
 describe('multiply()', () => {
     it('should multiply positive numbers together');
 
-    it('should divide positive and negative numbers together');
+    it('should multiply positive and negative numbers together');
 
-    it('should divide negative numbers together');
+    it('should multiply negative numbers together');
 });
