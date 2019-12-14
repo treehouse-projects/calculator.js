2fa2cb38112fe8267cb05562723fe64e5132ddc2
so0oshiance <mohamad.farhany@gmail.com>
2019-12-14 15:50:36 +0330

    implement multiply

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 'use strict';
 
 function add(a, b) {
-    return a + b;
+  return a + b;
 }
 
 function subtract(a, b) {
-    return a - b;
+  return a - b;
 }
 
 function divide(a, b) {
-    return a / b;
+  return a / b;
+}
+function multiply(a, b) {
+  return a * b;
 }
 
 module.exports = {
-    add,
-    subtract,
-    divide
+  add,
+  subtract,
+  divide,
+  multiply
 };
